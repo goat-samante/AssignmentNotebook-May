012b84f1fc45d676411b98f243b6726416b17869
Samante <samante@bsd220.org>
2025-04-27 11:04:55 -0500

    Initial Commit

---
+++ AssignmentNotebook May/AssignmentNotebook_MayApp.swift
@@ -0,0 +1,17 @@
+//
+//  AssignmentNotebook_MayApp.swift
+//  AssignmentNotebook May
+//
+//  Created by Samuel Amante on 4/27/25.
+//
+
+import SwiftUI
+
+@main
+struct AssignmentNotebook_MayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AssignmentNotebook MayTests/AssignmentNotebook_MayTests.swift
@@ -0,0 +1,17 @@
+//
+//  AssignmentNotebook_MayTests.swift
+//  AssignmentNotebook MayTests
+//
+//  Created by Samuel Amante on 4/27/25.
+//
+
+import Testing
+@testable import AssignmentNotebook_May
+
+struct AssignmentNotebook_MayTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
